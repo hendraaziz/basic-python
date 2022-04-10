738e3978dbccad5eadac28217356bda2cb563c6c
hendraaziz <hendraaziz80@gmail.com>
2022-04-10 14:41:23 +0700

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#input from user
+nama = input("Perkenalkan Nama kamu: ")
+print(f"Hallo, {nama} !")
+x = input("masukkan X: ")
+y = input("masukkan Y: ")
+print(f"Halo {nama}, Hasil Jumlah X + Y adalah: {int(x)+int(y)}")
