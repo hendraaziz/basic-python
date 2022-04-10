50ecb5c5f1f85b79fea553b002e2bba89a2ffb70
hendraaziz <hendraaziz80@gmail.com>
2022-04-10 15:15:21 +0700

    bool and list

---
+++ boolean.py
@@ -0,0 +1,7 @@
+#
+bil_1 = 12
+bil_2 = 10
+
+if bil_1 < bil_2 :
+    print(f"Bilangan {bil_1} lebih kecil dari bilangan {bil_2}")
+else: print(f"Bilangan {bil_1} lebih besar dari bilangan {bil_2}")
---
+++ list.py
@@ -0,0 +1,11 @@
+from xxlimited import foo
+
+
+foods = []
+foods.append("Ayam Bakar")
+foods.append("Rendang")
+foods.append("Bakso")
+print(len(foods))
+print(foods)
+for i in foods:
+    print(i)
