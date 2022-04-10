038c1443f53106376d10bea6ef38c3fcb89a9daa
hendraaziz <hendraaziz80@gmail.com>
2022-04-10 14:02:59 +0700

    trial print

---
+++ first-python.py
@@ -0,0 +1,8 @@
+#hello
+print("Hello Indonesia!!")
+nama = "Aziz Hendra"
+umur = 21
+isWorker = True
+print(f"Nama saya: {nama} \n Tipe dari variabel nama: {type(nama)}")
+print(f"Umur {umur}")
+print(f"isWorker?: {isWorker}")
