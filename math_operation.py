11fef4780f73a0e9af36b4900eb6de72e54d7e81
hendraaziz <hendraaziz80@gmail.com>
2022-04-10 14:18:34 +0700

    math operation

---
+++ math_operation.py
@@ -0,0 +1,18 @@
+from pickletools import int4
+from turtle import pen
+
+
+x: int = 10
+y: int = 2
+z: int = 3
+
+#operasi math
+#penjumlahan
+penjumlahan: int = x+y+z
+print(f"penjumlahan: {penjumlahan}")
+
+#pengurangan
+kurang: int = x-y-z
+print(f"Pengurangan: {kurang}")
+
+#dst
